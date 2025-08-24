bee1f0eb65c7f84cb00e2ea19f7a351252da04d7
benoy_Judy <benoyjudy@gmail.com>
2025-08-24 12:20:19 +0530

    migrate,created super user, install django restframework

---
+++ README.md
@@ -0,0 +1,54 @@
+# Upskillr-find the right mentor app
+
+
+## Idea behind the app and what real world problem does it solve
+
+
+
+## structure
+
+
+
+
+# Project Title
+
+A brief description of what this project does and who it's for.
+
+---
+
+## 🚀 Demo
+
+Include a link to a live demo or a screenshot.
+
+
+
+## 🛠️ Tech Stack
+
+**Frontend:** React, JavaScript and tailwind css and some 
+**Backend:** Django, Django REST Framework  
+**Database:** SQLite / PostgreSQL  
+**Other:** Axios, React Router
+
+---
+
+## 📁 Project Structure
+
+Upskillr/
+├── server/ # Django app
+├── client/ # React app
+├── README.md
+
+
+
+step
+
+## python manage.py migrate
+    to use djangos default user model
+
+## python manage.py createsuperuser
+
+## installing django restframework
+
+
+
+
--- server/Upskillr_project_main/settings.py
+++ server/Upskillr_project_main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6shgu&hnvi9x6n_x^e@84o5^i7jsnttwf%3*t31bv=y+t@*95!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     'rest_framework',
+     'Accounts_app',
+     "Api_app",
 ]
 
 MIDDLEWARE = [
